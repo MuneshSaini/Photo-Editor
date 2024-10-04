b6d60c93bd499e36fa6141cfdd96ed0ea8c79a87
Munesh Saini <136725215+MuneshSaini@users.noreply.github.com>
2024-10-05 01:18:31 +0530

    First Init

---
+++ app/src/main/java/com/burhanrashid52/photoediting/PhotoApp.kt
@@ -0,0 +1,17 @@
+package com.burhanrashid52.photoediting
+
+import android.app.Application
+
+
+class PhotoApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        photoApp = this
+    }
+
+    companion object {
+        var photoApp: PhotoApp? = null
+            private set
+        private val TAG = PhotoApp::class.java.simpleName
+    }
+}
---
+++ app/src/main/java/com/burhanrashid52/photoediting/base/BaseFragment.kt
@@ -0,0 +1,5 @@
+package com.burhanrashid52.photoediting.base
+
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment(layoutId: Int) : Fragment(layoutId)
